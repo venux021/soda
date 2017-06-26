e9095fa9dcf0fa6ae46eb2e5228fec99fab39714
Tang Qiming <tangqiming@tigerknows.com>
2017-06-26 11:47:13 +0800

    zcy: another c9/q20

--- zcy/c9/q20.py
+++ zcy/c9/q20.py
@@ -44,9 +44,49 @@ def found(arr, k):
 
     return -1
 
+def find_k(arr, k):
+    n = len(arr)
+    low = 0
+    high = n - 1
+    while low <= high:
+        mid = (low + high) / 2
+        if arr[mid] == k:
+            return mid
+        if arr[mid] == arr[low] and arr[mid] == arr[high]:
+            while arr[low] == arr[mid] and low < mid:
+                low += 1
+            if low == mid:
+                low = mid + 1
+                continue
+
+        if arr[mid] != arr[low]:
+            if arr[mid] > arr[low]:
+                if k >= arr[low] and k < arr[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if k > arr[mid] and k <= arr[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        elif arr[mid] != arr[high]:
+            if arr[mid] < arr[high]:
+                if k > arr[mid] and k <= arr[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            else:
+                if k >= arr[low] and k < arr[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+
+    return -1
+
 def test(arr, k):
     print 'arr: {}, k: {}'.format(arr, k)
-    print 'result: {}'.format(found(arr, k))
+    print 'result: {}'.format(find_k(arr, k))
 
 def main():
     '''在有序旋转数组中找到一个数'''
