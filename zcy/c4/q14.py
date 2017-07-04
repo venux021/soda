adc409452f6ae025c9b67c182b4161fda6f49dda
Tang Qiming <venux021@outlook.com>
2017-07-04 21:12:37 +0800

    zcy: c4 rearrange

--- zcy/c4/q14.py
+++ zcy/c4/q14.py
@@ -2,38 +2,49 @@
 # -*- coding: utf-8 -*-
 import sys
 
-def jump(arr):
-    n = len(arr)
-    if n == 0:
-        return -1
-    elif n == 1:
+def s(arr):
+    if len(arr) == 1:
         return 0
-    J = 0
-    limit = 0
-    farest = arr[0]
+    else:
+        return min(f(arr[1:]), f(arr[:-1]))
+
+def f(arr):
+    if len(arr) == 1:
+        return arr[0]
+    else:
+        return max(arr[0] + s(arr[1:]), arr[-1] + s(arr[:-1]))
+
+def win(arr):
+    return max(f(arr), s(arr))
+
+def winx(arr):
+    n = len(arr)
+    fm = [[0] * n for i in range(n)]
+    sm = [[0] * n for i in range(n)]
+    for i in range(n):
+        fm[i][i] = arr[i]
+
     for i in range(1,n):
-        if i > limit:
-            J += 1
-            limit = farest
-            if limit < i:
-                return -1
-            elif limit >= n-1:
-                return J
-        farest = max(farest, arr[i] + i)
+        r = 0
+        c = i
+        while c < n:
+            fm[r][c] = max(arr[c] + sm[r][c-1], arr[r] + sm[r+1][c])
+            sm[r][c] = min(fm[r][c-1], fm[r+1][c])
+            c += 1
+            r += 1
+
+    return max(fm[0][n-1], sm[0][n-1])
 
 def test(arr):
     print 'arr: {}'.format(arr)
-    print 'jump: {}'.format(jump(arr))
+    print 'win: {}'.format(win(arr))
+    print 'winx: {}'.format(winx(arr))
 
 def main():
-    '''跳跃游戏'''
-    test([3,2,3,1,1,4])
-    test([3,2,3,2,1,0,2])
-    test([5,1])
-    test([0,2])
-    test([0])
-    test([1])
-    test([10])
+    '''纸牌博弈'''
+    test([1,2,100,4])
+    test([1,100,2])
+    test([1,8,7,0,1,2,5,1,0,3,3])
 
 if __name__ == '__main__':
     reload(sys)
--- zcy/c4/q15.py
+++ zcy/c4/q15.py
@@ -2,37 +2,38 @@
 # -*- coding: utf-8 -*-
 import sys
 
-def longest(arr):
-    L = 0
-    m = {}
-    for j in range(len(arr)):
-        i = arr[j]
-        if i not in m:
-            m[i] = 1
-            if i-1 in m:
-                A = m[i-1]
-                left_a = i-1 - m[i-1] + 1
-                right_a = i
-                len_a = m[i] + m[i-1]
-                L = max(L, len_a)
-                m[left_a] = m[right_a] = len_a
-            if i+1 in m:
-                B = m[i+1]
-                left_b = i - m[i] + 1
-                right_b = i+1 + B - 1
-                len_b = m[i] + m[i+1]
-                L = max(L, len_b)
-                m[left_b] = m[right_b] = len_b
-    return L
+def jump(arr):
+    n = len(arr)
+    if n == 0:
+        return -1
+    elif n == 1:
+        return 0
+    J = 0
+    limit = 0
+    farest = arr[0]
+    for i in range(1,n):
+        if i > limit:
+            J += 1
+            limit = farest
+            if limit < i:
+                return -1
+            elif limit >= n-1:
+                return J
+        farest = max(farest, arr[i] + i)
 
 def test(arr):
     print 'arr: {}'.format(arr)
-    print 'max: {}'.format(longest(arr))
+    print 'jump: {}'.format(jump(arr))
 
 def main():
-    '''数组中的最长连续序列'''
-    test([100,4,200,1,3,2])
-    test([9,10,4,12,1,11,3,8,2])
+    '''跳跃游戏'''
+    test([3,2,3,1,1,4])
+    test([3,2,3,2,1,0,2])
+    test([5,1])
+    test([0,2])
+    test([0])
+    test([1])
+    test([10])
 
 if __name__ == '__main__':
     reload(sys)
