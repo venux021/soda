1c150e99a8a1f64099acbb76ab8e3702b247e84b
Tang Qiming <venux021@outlook.com>
2017-07-07 00:05:03 +0800

    zcy: c4/q03_3

--- zcy/c4/q03_3.py
+++ zcy/c4/q03_3.py
@@ -67,15 +67,63 @@ def test(arr, k):
     print 'min com 2: {}'.format(min_cost_2(arr, k))
     print 'min com 3: {}'.format(min_cost_3(arr, k))
 
+def test2(arr, k):
+    print 'arr: {}, k: {}'.format(arr, k)
+    print 'min cost 1: {}'.format(min01_cost_1(arr, k))
+    print 'min cost 2: {}'.format(min01_cost_2(arr, k))
+
+def min01_cost_1(arr, k):
+    return do_min01_cost(arr, 0, k)
+
+def do_min01_cost(arr, p, k):
+    n = len(arr)
+    if p == n:
+        return 0 if k == 0 else -1
+    elif k == 0:
+        return 0
+    elif k < 0:
+        return -1
+
+    x = do_min01_cost(arr, p+1, k-arr[p])
+    y = do_min01_cost(arr, p+1, k)
+    if x == -1 and y == -1:
+        return -1
+    elif x == -1:
+        return y
+    elif y == -1:
+        return x + 1
+    else:
+        return min(x+1, y)
+
+def min01_cost_2(arr, k):
+    n = len(arr)
+    dp = [0] * (k+1)
+
+    for i in range(1,k+1):
+        if arr[0] == k:
+            dp[i] = 1
+        else:
+            dp[i] = 0x7fffffff
+
+    for i in range(1, n):
+        for j in range(k, 0, -1):
+            if j >= arr[i]:
+                dp[j] = min(dp[j], dp[j-arr[i]]+1)
+
+    return dp[k] if dp[k] < 0x7fffffff else -1
+
 def main():
     '''换钱的最少货币数'''
-    test([5,2,3], 20)
-    test([5,2,3], 0)
-    test([3,5], 2)
-    test([1,2,5,10,11,15,19], 87)
-    test([1,2,5,10,14,17,23], 137)
-    test([1,2,5,10,20,25,50,100], 180)
-    test([1,2,5,10,20,35,50,63,89,100], 8379)
+#    test([5,2,3], 20)
+#    test([5,2,3], 0)
+#    test([3,5], 2)
+#    test([1,2,5,10,11,15,19], 87)
+#    test([1,2,5,10,14,17,23], 137)
+#    test([1,2,5,10,20,25,50,100], 180)
+#    test([1,2,5,10,20,35,50,63,89,100], 8379)
+    test2([1,5,3,1,3,5,3,4,5,1,1], 21)
+    test2([3,2,1,2,2,6,2,2,9], 10)
+    test2([3,6,2,4,5,6,7,8,2,4,6,7,8,3,1], 37)
 
 if __name__ == '__main__':
     reload(sys)
