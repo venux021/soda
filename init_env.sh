761d8ac3f49e82283df36b6d42093b993c6ed24b
Tang Qiming <tangqiming@tigerknows.com>
2017-08-08 16:28:44 +0800

    soda: new command to create file

--- init_env.sh
+++ init_env.sh
@@ -1,2 +1,6 @@
 SELF_DIR=$(pwd)
 echo $PYTHONPATH | grep -q $SELF_DIR || export PYTHONPATH="$SELF_DIR:$PYTHONPATH"
+echo "Add $SELF_DIR to PYTHONPATH"
+
+bin_dir=$SELF_DIR/bin
+export PATH="$bin_dir:$PATH"
