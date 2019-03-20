e7ef1876eca61a5bce87cb4dcc63a65010242dc3
Tang Qiming <tangqimingx@126.com>
2019-03-20 11:44:06 +0800

    zcy2: finish c5/q10

--- zcy2/c5/q10.py
+++ zcy2/c5/q10.py
@@ -25,6 +25,28 @@ def inplace_blank(arr):
             p -= 3
         j -= 1
 
+def move_star_left(s):
+    buf = list(s)
+    n = len(s)
+    j = n - 1
+    while j >= 0 and buf[j] != '*':
+        j -= 1
+    if j < 0:
+        return s
+    i = j
+    while i >= 0:
+        if buf[i] != '*':
+            buf[j] = buf[i]
+            buf[i] = '*'
+            j -= 1
+        i -= 1
+    return ''.join(buf)
+
+@testwrapper
+def test2(s):
+    print(s)
+    print(move_star_left(s))
+
 @testwrapper
 def test(s):
     n = len(s)
@@ -35,6 +57,10 @@ def test(s):
     print(''.join(filter(None, buf)))
 
 def main():
+    test2('12**345')
+    test2('***')
+    test2('*123*')
+    test2('123')
     test('a b  c')
     test('  ab c ')
     test('     a')
