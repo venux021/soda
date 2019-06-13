7a45cba1e7e822de36ba384ec3f15570ea70ddfa
Tang Qiming <tangqimingx@126.com>
2019-06-13 18:24:37 +0800

    update leetcode headers

--- leetcode/include/leetcode/list.h
+++ leetcode/include/leetcode/list.h
@@ -1,11 +1,56 @@
+#ifndef _LEETCODE_LIST
+#define _LEETCODE_LIST
+
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
+
+#include "array.h"
+
+namespace leetcode {
 
 struct ListNode {
     int val;
     ListNode *next;
     ListNode(int x) : val(x), next(NULL) {}
+};
+
+class List {
+public:
+
+    static ListNode *load(istream &in) {
+        string line;
+        return getline(in, line) ? load(line) : nullptr;
+    }
+    
+    static ListNode *load(string input) {
+        // Generate list from the input
+        vector<int> list = Array::loadIntArray(input);
+
+        // Now convert that list into linked list
+        ListNode* dummyRoot = new ListNode(0);
+        ListNode* ptr = dummyRoot;
+        for(int item : list) {
+            ptr->next = new ListNode(item);
+            ptr = ptr->next;
+        }
+        ptr = dummyRoot->next;
+        delete dummyRoot;
+        return ptr;
+    }
+
+    static string toString(ListNode *node) {
+        if (node == nullptr) {
+            return "[]";
+        }
+
+        string result;
+        while (node) {
+            result += to_string(node->val) + ", ";
+            node = node->next;
+        }
+        return "[" + result.substr(0, result.length() - 2) + "]";
+    }
 };
 
 struct ListHelper {
@@ -29,3 +74,7 @@ struct ListHelper {
         cout << endl;
     }
 };
+
+} // namespace leetcode
+
+#endif
