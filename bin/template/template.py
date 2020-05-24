85f73202a44b1e405ff90d91d584599897202d28
Tang Qiming <tangqiming@tigerknows.com>
2020-05-24 21:42:17 +0800

    new test decorator: sodatest

--- bin/template/template.py
+++ bin/template/template.py
@@ -6,10 +6,29 @@ import sys
 from sodacomm.bitree import *
 from sodacomm.graph import *
 from sodacomm.linklist import *
-from sodacomm.tools import testwrapper
+from sodacomm.tools import simpletest, sodatest, testrun
+
+# Solution here
+def solution():
+    pass
 
 def main():
+    # testrun(solution, a, b, c, d=e, f=g)
     pass
+
+# Template for simple test
+#@simpletest
+#def test(*args):
+#    print(*args)
+#    res = solution(*args)
+#    print(res)
+
+# Template for complex test
+#@sodatest(repeat=5)
+#def test(*args,**kwargs):
+#    print('input:', args, kwargs)
+#    res = solution(*args, **kwargs)
+#    print('result:' res)
 
 if __name__ == '__main__':
     main()
--- sodacomm/tools.py
+++ sodacomm/tools.py
@@ -1,6 +1,43 @@
-def testwrapper(func):
+import functools
+import time
+
+__test_number = 0
+
+def execute(func, args, kwargs, *, show_args=True, show_result=True):
+    global __test_number
+    __test_number += 1
+    print(f'**[{__test_number}]**')
+    if show_args:
+        if args:
+            print('args:', args)
+        if kwargs:
+            print('kwargs:', kwargs)
+    t1 = time.time()
+    res = func(*args, **kwargs)
+    t2 = time.time()
+    if show_result:
+        print(f'result:', res)
+    print(f'----')
+    print(f'{(t2-t1)*1000:.3f} ms\n')
+    return res
+
+def simpletest(func):
+    @functools.wraps(func)
     def _test(*args, **kwargs):
-        r = func(*args, **kwargs)
-        print('----')
-        return r
+        return execute(func, args, kwargs, show_args=False, show_result=False)
     return _test
+
+testwrapper = simpletest
+
+def sodatest(repeat=1,show_args=True,show_result=True):
+    def _wrapper(func):
+        @functools.wraps(func)
+        def runner(*args, **kwargs):
+            for i in range(max(1,repeat)):
+                res = execute(func, args, kwargs, show_args=show_args, show_result=show_result)
+            return res
+        return runner
+    return _wrapper
+
+def testrun(func, *args, **kwargs):
+    execute(func, args, kwargs)
