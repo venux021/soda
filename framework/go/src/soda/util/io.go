f68134b008bc671533c58e5de494de3ab7a08f47
Tang Qiming <tangqiming@didiglobal.com>
2021-06-28 20:58:59 +0800

    go simple logger

--- framework/go/src/soda/util/io.go
+++ framework/go/src/soda/util/io.go
@@ -3,6 +3,18 @@ package util
 import "fmt"
 import "os"
 
+type SimpleLogger struct {}
+
+var Logger SimpleLogger = SimpleLogger{}
+
+func (g *SimpleLogger) Finfo(format string, a ...interface{}) {
+    fmt.Fprintf(os.Stderr, "[INFO] " + format + "\n", a...)
+}
+
+func (g *SimpleLogger) Ferror(format string, a ...interface{}) {
+    fmt.Fprintf(os.Stderr, "[ERROR] " + format + "\n", a...)
+}
+
 func PrintErr(format string, a ...interface{}) (n int, err error) {
     return fmt.Fprintf(os.Stderr, format, a...)
 }
